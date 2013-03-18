1a14df7f4b4ce54907abe86160c751b8eb1b8d40
Aaron Rama and Rich Wallett <aaronrama@gmail.com + rich.wallett@gmail.com>
2013-03-18 13:21:19 -0700

    Add methods to make board and create minefield.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,4 +1,39 @@
 class Minesweeper
+  def initialize
+    @board = make_board(:small)
+  end
+
+  def make_board(size)
+    small_board = []
+    if size == :small
+      row = []
+      9.times {row << "_"}
+      9.times {small_board << row.dup}
+      small_board
+    end
+    10.times {small_board[rand(9)][rand(9)] = "m"}
+    small_board
+  end
+
+  def display_board
+    display_board = []
+    @board.dup.each do |row|
+      row.map! do |el|
+        if el == "m"
+          el = "_"
+        else
+          el = "_"
+        end
+      end
+      display_board << row
+    end
+    display_board.each { |row| p row}
+  end
+
+
 
 
 end
+
+mine = Minesweeper.new
+mine.display_board
