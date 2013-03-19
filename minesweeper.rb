6ed4310f38524bb6a42c9153db7fa83bef23abcb
Aaron Rama and Rich Wallett <aaronrama@gmail.com + rich.wallett@gmail.com>
2013-03-18 18:07:10 -0700

    Saves games and works

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,25 +1,33 @@
 require 'debugger'
+require 'yaml'
 
 class Minesweeper
   attr_reader :display_board, :board
-  def initialize
-    @board = make_board(:small)
+  def initialize(size)
+    @board = make_board(size)
     @display_board = initial_display_board
     @history = []
+    @start_time = Time.now
     play
   end
 
   def play
-    while game_won? == false
+    forfeit = nil
+    while game_won? == false && forfeit == nil
       puts "Here is the minefield:\n"
-      @board.each {|row| p row}
-      @display_board.each_with_index {|row, index| p index, row}
+      @display_board.each{|row| p row}
       puts ''
-      puts "Would you like to dig or flag? 1-Dig, 2-Flag"
+      puts "Would you like to dig or flag? 1-Dig, 2-Flag, 3-Save, 4-Quit"
       command = gets.chomp.to_i
+      case command
+        when 3 then save_game
+        when 4
+          forfeit = "quit"
+          return
+      end
       puts "Where would you like to dig/flag? x,y"
       coords = gets.chomp
-      if is_mine?(coords[0].to_i, coords[2].to_i)
+      if command == 1 && is_mine?(coords[0].to_i, coords[2].to_i)
         p "You lose, Shame"
         return
       else
@@ -29,20 +37,34 @@ class Minesweeper
         end
       end
     end
-    "Congrats you cleared the minefield."
+    p "Congrats you cleared the minefield."
+    end_time = Time.now
+    p "It took you: #{end_time - @start_time} seconds"
   end
 
+
+  def save_game
+    save = self.to_yaml
+    File.open("save.yaml", "w") do |f|
+      f.puts save
+    end
+    puts "Game saved."
+  end
+
+
   def make_board(size)
-    small_board = []
+    board = []
     if size == :small
       row = []
       9.times {row << "_"}
-      9.times {small_board << row.dup}
-      small_board
+      9.times {board << row.dup}
+      10.times {board[rand(9)][rand(9)] = "m"}
+    elsif size == :large
+      16.times {row << "_"}
+      16.times {board << row.dup}
+      40.times {board[rand(16)][rand(16)] = "m"}
     end
-    1.times {small_board[rand(9)][rand(9)] = "m"}
-    small_board.each {|row| p row}
-    small_board
+    board
   end
 
   def initial_display_board
@@ -69,15 +91,15 @@ class Minesweeper
   end
 
   def is_mine?(x,y)
-    if @board[y][x] == "m"
+    if @board[x][y] == "m"
       return true
     end
   end
 
   def get_neighbors(x,y)
-    neighbors = [[y-1 , x-1],[y-1 , x], [y-1 , x+1],
-                      [y , x-1],[y,x],[y , x+1],
-                      [y+1 , x-1], [y+1 , x],[y+1 , x+1]]
+    neighbors = [[x-1 , y-1],[x-1 , y], [x-1 , y+1],
+                      [x , y-1],[x,y],[x , y+1],
+                      [x+1 , y-1], [x+1 , y],[x+1 , y+1]]
     neighbors.select! do |coord|
       # Find all possible moves from coordinates
       (coord[0] >= 0 && coord[0] <= 8)  &&
@@ -87,11 +109,11 @@ class Minesweeper
   end
 
   def reveal(x,y)
-    neighbors = get_neighbors(y,x)
+    neighbors = get_neighbors(x,y)
     if count_mines(neighbors) != 0
-      @display_board[y][x] = count_mines(neighbors).to_s unless              @board[x][y] == "m"
+      @display_board[x][y] = count_mines(neighbors).to_s unless              @board[x][y] == "m"
     else
-      @display_board[y][x] = "X" unless @board[y][x] == "m"
+      @display_board[x][y] = "X" unless @board[x][y] == "m"
       neighbors.each do |neighbor|
         next if @history.include?(neighbor)
         @history << neighbor
@@ -110,9 +132,19 @@ class Minesweeper
   end
 
   def flag(x,y)
-    @display_board[y][x] = "F"
+    @display_board[x][y] = "F"
   end
 
+end
+
+class PlayMinesweeper
+  def initialize(command, size=:small, game_instance = nil)
+    if command == :new
+      Minesweeper.new(size)
+    elsif command == :load
+      YAML::load(game_instance)
+    end
+  end
 end
 
 class Array
@@ -131,6 +163,6 @@ class Array
   end
 end
 
-mine = Minesweeper.new
+mine = PlayMinesweeper.new(:new, :small)
 
 
