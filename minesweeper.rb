3d82e1ffc8f1eb56599fb206d49f87e578f07eba
Aaron Rama and Rich Wallett <aaronrama@gmail.com + rich.wallett@gmail.com>
2013-03-18 17:14:14 -0700

    made gameplay method.  nearly working

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,6 +6,30 @@ class Minesweeper
     @board = make_board(:small)
     @display_board = initial_display_board
     @history = []
+    play
+  end
+
+  def play
+    while game_won? == false
+      puts "Here is the minefield:\n"
+      @board.each {|row| p row}
+      @display_board.each_with_index {|row, index| p index, row}
+      puts ''
+      puts "Would you like to dig or flag? 1-Dig, 2-Flag"
+      command = gets.chomp.to_i
+      puts "Where would you like to dig/flag? x,y"
+      coords = gets.chomp
+      if is_mine?(coords[0].to_i, coords[2].to_i)
+        p "You lose, Shame"
+        return
+      else
+        case command
+        when 1 then reveal(coords[0].to_i, coords[2].to_i)
+        when 2 then flag(coords[0].to_i, coords[2].to_i)
+        end
+      end
+    end
+    "Congrats you cleared the minefield."
   end
 
   def make_board(size)
@@ -16,7 +40,8 @@ class Minesweeper
       9.times {small_board << row.dup}
       small_board
     end
-    10.times {small_board[rand(9)][rand(9)] = "m"}
+    1.times {small_board[rand(9)][rand(9)] = "m"}
+    small_board.each {|row| p row}
     small_board
   end
 
@@ -32,12 +57,12 @@ class Minesweeper
       end
       display_board << row
     end
-    p @board.each {|row| p row}
+    #p @board.each {|row| p row}
     display_board
   end
 
   def game_won?
-    @board.each do |row|
+    @display_board.each do |row|
       return false if row.include?("_")
     end
     true
@@ -51,7 +76,7 @@ class Minesweeper
 
   def get_neighbors(x,y)
     neighbors = [[y-1 , x-1],[y-1 , x], [y-1 , x+1],
-                      [y , x-1],[y , x+1],
+                      [y , x-1],[y,x],[y , x+1],
                       [y+1 , x-1], [y+1 , x],[y+1 , x+1]]
     neighbors.select! do |coord|
       # Find all possible moves from coordinates
@@ -62,17 +87,17 @@ class Minesweeper
   end
 
   def reveal(x,y)
-    neighbors = get_neighbors(x,y)
+    neighbors = get_neighbors(y,x)
     if count_mines(neighbors) != 0
-      @display_board[y][x] = count_mines(neighbors).to_s
+      @display_board[y][x] = count_mines(neighbors).to_s unless              @board[x][y] == "m"
     else
+      @display_board[y][x] = "X" unless @board[y][x] == "m"
       neighbors.each do |neighbor|
         next if @history.include?(neighbor)
         @history << neighbor
         reveal(neighbor[0], neighbor[1])
       end
     end
-    return [x,y,count_mines(neighbors)]
   end
 
   def count_mines(neighbors)
@@ -87,7 +112,6 @@ class Minesweeper
   def flag(x,y)
     @display_board[y][x] = "F"
   end
-
 
 end
 
@@ -108,7 +132,5 @@ class Array
 end
 
 mine = Minesweeper.new
-mine.reveal(3,4)
-p mine.display_board.each {|row| p row}
-p mine.board[4][4]
+
 
